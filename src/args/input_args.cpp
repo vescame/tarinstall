f37a3efa063c81a2586cce907ce0ff901a045ca8
Vinicius Escame <46465751+vescm@users.noreply.github.com>
2022-03-25 12:31:11 +0000

    moved include outside src folder (#1)
    * moved include outside src folder
    
    * impl generic shared and unique ptrs
    
    * refactor parser to safe pointers

--- src/args/input_args.cpp
+++ src/args/input_args.cpp
@@ -1,4 +1,4 @@
-#include "../include/args/input_args.hpp"
+#include "../../include/args/input_args.hpp"
 
 const std::vector<std::string>
 args::handle(char** argv, int argc) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include "include/files/copy_file.hpp"
-#include "include/args/input_args.hpp"
-#include "include/logging/logger.hpp"
-
-void
-copyfiles(const char* from, const char* to)
-{
-  files::copy(from, to);
-}
+#include "../include/files/copy.hpp"
+#include "../include/args/input_args.hpp"
+#include "../include/logging/logger.hpp"
+#include "../include/pointers/safe_ptrs.hpp"
 
 int
 main(int argc, char** argv)
 {
-  if (argc < 2) logging::error("must have two args $src $dst", 1);
+  if (argc < 3) logging::error("must have two args $src $dst", 1);
   auto args = args::handle(argv, argc);
-  copyfiles(args.at(0).c_str(), args.at(1).c_str());
+
+  auto from = args[0].c_str();
+  auto to = args[1].c_str();
+
+  files::copy_structure(from, to);
 
   return 0;
 }
